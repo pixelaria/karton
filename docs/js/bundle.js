b2422820d2c8b8a1ed9a0014082d1c3f8a116d58
pixelaria <info@pixelaria.ru>
2019-09-29 15:14:50 +0300

    Header + mobile menu

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,2 @@
 
-$(function(){console.log('init 0.1');});
+$(function(){console.log('init 0.1');$('#nav-toggler').click(function(e){target=$(this).data('target');$('#'+target).toggleClass('nav__list--active');});});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,10 @@
 $(function (){
   console.log('init 0.1');
+  
+  $('#nav-toggler').click(function(e){
+    target = $(this).data('target');
+    $('#'+target).toggleClass('nav__list--active');
+  });  
+
 });
 
